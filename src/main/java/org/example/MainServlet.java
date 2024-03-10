30f0728761c648aef65570ad5ff2779c6a02e952
up156 <up156@yandex.ru>
2024-03-10 19:49:17 +0300

    WeatherStation created, returns forecast for given input city and number of days.

--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -2,22 +2,58 @@ package org.example;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/weather")
 @Slf4j
 public class MainServlet extends HttpServlet {
 
+    private final WeatherStation weatherStation;
+
+    public MainServlet() {
+        this.weatherStation = new WeatherStation();
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         log.info("working with request {}, {}", req.getRequestURL(), req.getMethod());
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        String city = req.getParameter("city");
+        String daysInput = req.getParameter("days");
+        Integer days;
+
+        if (city == null) {
+            printWriter.write("Пожалуйста, введите город параметром \"city\". " +
+                    "</b> Параметр \"days\" для количества дней (от 1 до 5)" +
+                    "Например, /weather?city=moscow&days=3");
+            return;
+        }
+        log.info("working with weather for city input: {} for days input: {}", city, daysInput);
+
+        daysInput = daysInput == null ? "1" : daysInput;
+        try {
+            days = Integer.parseInt(daysInput);
+            if (days > 5 || days < 1) throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            printWriter.write("Введите количество дней числом от 1 до 5");
+            printWriter.close();
+            return;
+        }
+
+        printWriter.println("<b>Погода в городе: " + city + "<br />" + "<br /> + </b>");
+
+        for (String s : weatherStation.getWeather(city, days)) {
+            printWriter.println(s + "<br />");
+        }
+        printWriter.close();
 
     }
 }
--- src/main/java/org/example/WeatherStation.java
+++ src/main/java/org/example/WeatherStation.java
@@ -1,5 +1,151 @@
 package org.example;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class WeatherStation {
 
+    private final static String key = "";
+    private final static String WEATHER_NOW = "https://api.openweathermap.org/data/2.5/weather?q=&lang=ru&units=metric&appid=" + key;
+    private final static String WEATHER_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?q=&lang=ru&units=metric&appid=" + key;
+
+    public WeatherStation() {
+    }
+
+    public List<String> getWeather(String city, Integer days) {
+
+        try {
+            List<String> weatherReply = getWeatherReply(city);
+            if (weatherReply.isEmpty()) {
+                return Collections.singletonList("нет информации для этого города");
+            }
+
+            String now = weatherReply.get(0);
+            String forecast = weatherReply.get(1);
+            JSONParser parser = new JSONParser();
+            org.json.simple.JSONObject object = (org.json.simple.JSONObject) parser.parse(now);
+            org.json.simple.JSONObject main = (org.json.simple.JSONObject) object.get("main");
+            org.json.simple.JSONObject sys = (org.json.simple.JSONObject) object.get("sys");
+            org.json.simple.JSONObject weather = (JSONObject) ((JSONArray) object.get("weather")).get(0);
+            List<String> result = new ArrayList<>(List.of("\uD83E\uDD99 Температура  сейчас: "
+                            + (Math.round(Double.parseDouble(main.get("temp").toString()) * 10) / 10.0) + "°",
+                    getEmoji(weather.get("icon").toString()) + " На улице " + weather.get("description"),
+                    "☀" + "☀" + "☀" + " Восход солнца: " + convertEpochToLocalTime(sys.get("sunrise").toString()),
+                    "\uD83C\uDF1D\uD83C\uDF1D\uD83C\uDF1D ",
+                    "Заход солнца: " + convertEpochToLocalTime(sys.get("sunset").toString()), "", "",
+                    "⚡⚡", "", ""));
+
+            org.json.simple.JSONObject objectForecast = (org.json.simple.JSONObject) parser.parse(forecast);
+            org.json.simple.JSONArray list = (org.json.simple.JSONArray) objectForecast.get("list");
+            LocalDate temp = LocalDate.now();
+            result.add("Прогноз на: " + temp.format(DateTimeFormatter.ofPattern("dd/MM")));
+            temp = temp.plus(1, ChronoUnit.DAYS);
+
+            for (int j = 0; j < 40; j++) {
+
+                org.json.simple.JSONObject weatherForecast = (JSONObject) ((JSONArray) ((JSONObject) list.get(j)).get("weather")).get(0);
+                Long dt = (Long) ((JSONObject) list.get(j)).get("dt");
+                LocalDateTime dateOfForecast = convertEpochToLocalTime(dt.toString());
+                if (dateOfForecast.isBefore(LocalDateTime.now()
+                        .plus(days, ChronoUnit.DAYS).plus(4, ChronoUnit.HOURS))) {
+                    if (dateOfForecast.toLocalDate().isEqual(ChronoLocalDate.from(temp))) {
+                        result.add("");
+                        result.add("Прогноз на: " + dateOfForecast.format(DateTimeFormatter.ofPattern("dd/MM")));
+                        result.add("");
+                        temp = temp.plus(1, ChronoUnit.DAYS);
+                    }
+                    org.json.simple.JSONObject mainForecast = (JSONObject) ((JSONObject) list.get(j)).get("main");
+                    String string = dateOfForecast.format(DateTimeFormatter.ofPattern("HH:mm dd/MM")) + " - " +
+                            getEmoji(weatherForecast.get("icon").toString()) + " " +
+                            (Math.round(Float.parseFloat(mainForecast.get("temp").toString()) * 10)) / 10.0 +
+                            "°" + " " + weatherForecast.get("description") + "\r\n";
+                    result.add(string);
+                }
+            }
+            System.out.println(result);
+            return result;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return Collections.singletonList("что-то сломалось");
+        }
+    }
+
+    private List<String> getWeatherReply(String city) {
+
+        List<String> replies = new ArrayList<>();
+        try {
+            city = URLEncoder.encode(city, StandardCharsets.UTF_8);
+            List<URL> list = List.of((new URL(new StringBuilder(WEATHER_NOW).insert(50, city).toString())),
+                    new URL(new StringBuilder(WEATHER_FORECAST).insert(51, city).toString()));
+            list.forEach(url -> {
+                try {
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Accept-Language", "en-US,ru;q=0.5");
+                    con.setConnectTimeout(5000);
+                    con.setReadTimeout(5000);
+                    con.setDoOutput(true);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+                    String inputLine;
+                    StringBuilder reply = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        reply.append(inputLine);
+                    }
+                    in.close();
+                    replies.add(reply.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        return replies;
+    }
+
+    private LocalDateTime convertEpochToLocalTime(String epoch) {
+
+        return Instant.ofEpochSecond(Long.parseLong(epoch))
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+
+    }
+
+    private String getEmoji(String icon) {
+        return switch (icon) {
+            case "01d" -> "☀";
+            case "01n" -> "\uD83C\uDF1A";
+            case "02d" -> "\uD83C\uDF24";
+            case "02n", "03d", "03n", "04d", "04n" -> "☁";
+            case "09d" -> "\uD83C\uDF26";
+            case "09n", "10d", "10n" -> "\uD83C\uDF28";
+            case "11d", "11n" -> "\uD83C\uDF29";
+            case "13d", "13n" -> "❄";
+            case "50d", "50n" -> "\uD83C\uDF2B";
+            default -> "";
+        };
+    }
 }
